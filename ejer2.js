9d80cfde0bdd442b7c4f0bef658cfd2905a35b28
agus0014 <agustingma87@gmail.com>
2023-06-12 22:35:53 -0300

    Initial commit

---
+++ ejer2.js
@@ -0,0 +1,6 @@
+function showalert (){ alert('¡hola, esta es una alerta desde javascript'); }
+function showalert (){ alert('¡hola, esta es una alerta desde javascript'); }
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")}) 
